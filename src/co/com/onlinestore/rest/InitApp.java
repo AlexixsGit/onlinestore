f78f8796ff768a7b759544421c3043dfde6f45cd
Alexis Ortiz <e.du1991@hotmail.com>
2016-10-07 15:45:51 -0500

    Se creo una clase llamada SessionModel Esta clase va almacenar todos los datos del cliente, ademas se esta obteniendo la ip del equipo que se loguea a la aplicacion.

--- src/co/com/onlinestore/rest/InitApp.java
+++ src/co/com/onlinestore/rest/InitApp.java
@@ -1,22 +1,44 @@
 package co.com.onlinestore.rest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Logger;
+import co.com.onlinestore.model.SessionModel;
 
 @Path("/InitApp")
 public class InitApp {
 
+	private SessionModel sessionModel;
+
+	public InitApp() {
+		System.out.println("Inicio aplicacion");
+		sessionModel = new SessionModel();
+		ip();
+	}
+
 	@Path("/writeLog")
 	public Response writeLog() {
-		System.out.println("Inicio aplicacion");
 		System.setProperty("logback.configurationFile", "C:\\log\\logback.xml");
 		Logger log = (Logger) LoggerFactory.getLogger(InitApp.class);
 		log.info("mensaje1");
 		log.error("Error1");
-		return Response.ok().build();
+		return Response.ok(this.sessionModel).build();
+	}
+
+	// Obtencion de la ip del equipo que ejecuta la aplicacion
+	private void ip() {
+		try {
+			sessionModel.setIp(InetAddress.getLocalHost());
+			System.out.println("Current IP address : " + sessionModel.getIp().getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
